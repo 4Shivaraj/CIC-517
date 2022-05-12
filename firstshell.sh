5a954c8f4651baf8097322f81deda5b307d42cad
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-05-12 08:31:25 +0530

    [started with code]

---
+++ firstshell.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+echo "welcome to shell script"
+
+#intialize a vaiable
+read -p "Enter your name " Fullname
+
+echo "Enter your city"
+read city
+
+#fullName="Shivaraj k"
+
+echo $fullname
+echo $city
